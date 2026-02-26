7e4d419ad50119baaddda7c1ac240b4088357e7e
Javier Ortiz Celsi <javierortizcelsi@gmail.com>
2026-02-26 09:11:33 -0300

    update config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  -- Add Catppuccin plugin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+    },
+  },
+
+  -- Configure LazyVim to use Catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
